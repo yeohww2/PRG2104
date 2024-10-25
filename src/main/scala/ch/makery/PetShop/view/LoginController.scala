2f1b047343f408cddc62046acb39a6303eff3b60
yeohww2 <139027337+yeohww2@users.noreply.github.com>
2024-10-25 23:04:46 +0800

    Update LoginController.scala

--- src/main/scala/ch/makery/PetShop/view/LoginController.scala
+++ src/main/scala/ch/makery/PetShop/view/LoginController.scala
@@ -1,1 +1,44 @@
+package ch.makery.PetShop.view
 
+import ch.makery.PetShop.MainApp
+import scalafx.scene.control.{Alert, Button, PasswordField, TextField}
+import scalafx.scene.control.Alert.AlertType
+import scalafxml.core.macros.sfxml
+
+
+
+@sfxml
+class LoginController(
+                       private val UsernameButton: TextField,
+                       private val PasswordButton: PasswordField,
+                       private val LoginButton: Button,
+                       private val CloseButton: Button
+                     ) {
+
+  //** Event handler for the login button
+  def handleLogin(): Unit = {
+    val username = UsernameButton.text.value
+    val password = PasswordButton.text.value
+
+    if (username == "admin" && password == "password") {
+      // Your login handling code here
+      println("Login successful!")
+      MainApp.showDashboardPage()
+
+    } else {
+      // Show an alert for invalid login
+      val alert = new Alert(AlertType.Error) {
+        title = "Invalid Login"
+        headerText = "Invalid username or password"
+        contentText = "Please enter valid credentials."
+      }
+      alert.showAndWait()
+    }
+  }
+
+  //** Event handler for the close button
+  def handleClose(): Unit = {
+    MainApp.closeApplication()
+  }
+
+}
